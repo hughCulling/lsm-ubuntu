93971191450e50594d7ca54c1d40f24f846c9830
Hugh Culling <hughculling@icloud.com>
2024-01-24 23:26:01 +0000

    test

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Define a route
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/html/index.html");
+  res.sendFile(path.join(__dirname, "public", "html", "index.html"));
 });
 
 const privateKey = fs.readFileSync(
