3517317cd562d75b0bb0ff865bf4011038178dd7
Hugh Culling <hughculling@icloud.com>
2024-01-24 23:32:28 +0000

    middleware

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const fs = require("fs");
 
 const app = express();
 
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Define a route
 app.get("/", (req, res) => {
