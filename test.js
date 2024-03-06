7487b38cdbdee2becced1738af68c17a450804b6
Hugh Culling <hughculling@icloud.com>
2024-03-06 15:20:25 +0000

    Added Selenium script.

---
+++ test.js
@@ -0,0 +1,14 @@
+import { Builder, By } from "selenium-webdriver";
+
+async function seleniumScript() {
+  // Start the session
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  // Take action on browser
+  await driver.get("https://live-stream-music.com");
+
+  // End the session
+  await driver.quit();
+}
+
+seleniumScript();
