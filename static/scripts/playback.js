bb9352e613233625421487335e30aa0dcf7a3348
Hugh Culling <hughculling@icloud.com>
2024-03-23 09:35:19 +0000

    Installed React.

--- static/scripts/playback.js
+++ static/scripts/playback.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Access the 'streamKey' value from the data attribute.
-  playbackUrl = document
-    .querySelector("script[data-playback-url]")
-    .getAttribute("data-playback-url");
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Access the 'streamKey' value from the data attribute.
+//   playbackUrl = document
+//     .querySelector("script[data-playback-url]")
+//     .getAttribute("data-playback-url");
 
-  // Use the 'streamKey' variable as needed.
-  console.log(playbackUrl);
+//   // Use the 'streamKey' variable as needed.
+//   console.log(playbackUrl);
 
-  if (IVSPlayer.isPlayerSupported) {
-    const player = IVSPlayer.create();
-    player.attachHTMLVideoElement(document.getElementById("video-player"));
-    player.load(`${playbackUrl}`);
-    player.play();
-  }
-});
+//   if (IVSPlayer.isPlayerSupported) {
+//     const player = IVSPlayer.create();
+//     player.attachHTMLVideoElement(document.getElementById("video-player"));
+//     player.load(`${playbackUrl}`);
+//     player.play();
+//   }
+// });
 
-let playbackUrl = "";
+// let playbackUrl = "";
 
 // if (IVSPlayer.isPlayerSupported) {
 //   const player = IVSPlayer.create();
@@ -23,3 +23,12 @@ let playbackUrl = "";
 //   player.load(`${playbackUrl}`);
 //   player.play();
 // }
+
+import { createRoot } from "react-dom/client";
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById("app"));
+root.render(<h1>Hello, world</h1>);
